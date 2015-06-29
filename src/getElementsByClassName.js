7101b363efb0ffc3fa28166393b1de24e7c3e707
gartenfeld <david@rosson.com.au>
2015-06-29 15:06:37 -0700

    Added getElementsByClassName; passes all test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
+  var results = [];
+
+  var classChecker = function(elem){
+  	if (elem.classList && elem.classList.contains(className)) {
+  		results.push(elem);
+  	}
+  };
+
+  var searchChildren = function(node){
+  	for (var i = 0; i<node.childNodes.length; i++){
+  	  classChecker(node.childNodes[i]);
+  	  searchChildren(node.childNodes[i]);
+  	}
+  };
+
+  classChecker(document.body);
+  searchChildren(document.body);
   // your code here
+  return results;
 };
